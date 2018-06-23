5f4f3a28a325e9439d00793e4ecf0ba92e57c619
Andy <anlaics2@gmail.com>
2018-06-23 14:09:42 -0700

    routing added and portfolio screen initialized

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import Navbar from './components/common/Navbar';
 import Footer from './components/common/Footer';
 import CoinList from './components/home/CoinList';
+import Portfolio from './components/portfolio/Portfolio';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar />
-        <div className="container">
-          <CoinList />
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={CoinList} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     );
   }
 }
