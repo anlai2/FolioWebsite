955443e428c5975e31a21d776b575f01719426e1
Andy <anlaics2@gmail.com>
2018-06-23 17:44:44 -0700

    number input for add assets screen

--- src/components/portfolio/AddAsset.js
+++ src/components/portfolio/AddAsset.js
@@ -13,50 +13,39 @@ class AddAsset extends Component {
         <div className="row">
           <div className="col-md-12">
             <p className="lead text-muted">Add Coins</p>
-            <div className="form-check">
-              <ul className="list-group">
-                {checked.map((coin, index) => (
-                  <li
-                    className="list-group-item"
-                    key={index}
-                    style={{ backgrounColor: '#00ff00' }}
-                  >
-                    <input
-                      className="form-check-input col-md-3"
-                      type="checkbox"
-                      value=""
-                      id="defaultCheck1"
-                      onChange={() => {
-                        checked.indexOf(coin.symbol) > -1
-                          ? this.setState({
-                              checked: [
-                                ...checked.slice(
-                                  0,
-                                  checked.indexOf(coin.symbol)
-                                ),
-                                ...checked.slice(
-                                  checked.indexOf(coin.symbol) + 1
-                                )
-                              ]
-                            })
-                          : this.setState({
-                              checked: [...checked, coin.symbol]
-                            });
-                      }}
-                    />
-                    <label className="form-check-label col-md-9">
-                      {coin.name}
-                    </label>
-                  </li>
-                ))}
-              </ul>
-              <input
-                type="submit"
-                value="Submit"
-                className="btn btn-info btn-block mt-4"
-                onClick={this.onSubmit}
-              />
-            </div>
+            <ul className="list-group">
+              {checked.map((coin, index) => (
+                <li className="list-group-item" key={index}>
+                  <label className="form-check-label col-md-9">
+                    {coin.name}
+                  </label>
+                  <input
+                    className="form-control col-md-3"
+                    placeholder="0.000000"
+                    value=""
+                    id="defaultCheck1"
+                    onChange={() => {
+                      checked.indexOf(coin.symbol) > -1
+                        ? this.setState({
+                            checked: [
+                              ...checked.slice(0, checked.indexOf(coin.symbol)),
+                              ...checked.slice(checked.indexOf(coin.symbol) + 1)
+                            ]
+                          })
+                        : this.setState({
+                            checked: [...checked, coin.symbol]
+                          });
+                    }}
+                  />
+                </li>
+              ))}
+            </ul>
+            <input
+              type="submit"
+              value="Submit"
+              className="btn btn-info btn-block mt-4"
+              onClick={this.onSubmit}
+            />
           </div>
         </div>
       </div>
